739d9a3ade34876b81ba17d0e0865f62580f92c4
Malek <malek@zeitspace.com>
2018-07-20 09:13:10 -0400

    Successful binary classification with 80% accuracy

--- ethnicclassify.py
+++ ethnicclassify.py
@@ -9,16 +9,45 @@ from keras.preprocessing.image import ImageDataGenerator
 trainPath='Train'
 testPath='Test'
 validPath='Valid'
+practiceTrainPath='PracticeTrain'
+practiceTestPath='PracticeTest'
+practiceValidPath='PracticeValid'
+trainBatches = ImageDataGenerator().flow_from_directory(trainPath, target_size=(224,224), classes=['African', 'American', 'Arab', 'Asian', 'Indian'], batch_size=64)
+testBatches = ImageDataGenerator().flow_from_directory(testPath, target_size=(224,224), classes=['African', 'American', 'Arab', 'Asian', 'Indian'], batch_size=32)
+validBatches = ImageDataGenerator().flow_from_directory(validPath, target_size=(224,224), classes=['African', 'American', 'Arab', 'Asian', 'Indian'], batch_size=32)
 
-trainBatches = ImageDataGenerator().flow_from_directory(trainPath, target_size=(224,224), classes=['Training Africa', 'Training America', 'Training Arab', 'Training Asian', 'Training Indian'], batch_size=32)
-testBatches = ImageDataGenerator().flow_from_directory(testPath, target_size=(224,224), classes=['Testing Africa', 'Testing America', 'Testing Arab', 'Testing Asian', 'Testing Indian'], batch_size=50)
-validBatches = ImageDataGenerator().flow_from_directory(validPath, target_size=(224,224), classes=['Valid Africa', 'Valid America', 'Valid Arab', 'Valid Asian', 'Valid Indian'], batch_size=30)
+practiceTrainBatches = ImageDataGenerator().flow_from_directory(practiceTrainPath, target_size=(224,224), classes=['Cats','Dogs'], batch_size=64)
+practiceTestBatches = ImageDataGenerator().flow_from_directory(practiceTestPath, target_size=(224,224), classes=['Cats','Dogs'], batch_size=32)
+practiceValidBatches = ImageDataGenerator().flow_from_directory(practiceValidPath, target_size=(224,224), classes=['Cats','Dogs'], batch_size=32)
 print((trainBatches))
 imgs, labels = next(trainBatches)
-model = Sequential()
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, 224, 224)
+else:
+    input_shape = (224, 224, 3)
 
-model.add(Conv2D(32,(3,3), activation='relu', input_shape=(224,224,3)))
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
-model.add(Dense(5, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit_generator(trainBatches, steps_per_epoch=5, validation_data=validBatches, validation_steps=5, epochs=50,verbose=2)
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Dense(2,activation='softmax'))
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit_generator(practiceTrainBatches, steps_per_epoch=5, validation_data=practiceValidBatches, validation_steps=5, epochs=50,verbose=2)
